9e6b2ffe8714ea6f1b91c6a1ba062851fdac5349
g-alexander <g.alexander.box@gmail.com>
2025-08-02 16:41:12 +0300

    Update windows build script

--- windows-build.bat
+++ windows-build.bat
@@ -1,6 +1,12 @@
+set PYO3_PYTHON C:\Python\Python38\python.exe
 C:\Python\Python38\python setup.py bdist_wheel
+set PYO3_PYTHON C:\Python\Python39\python.exe
 C:\Python\Python39\python setup.py bdist_wheel
+set PYO3_PYTHON C:\Python\Python310\python.exe
 C:\Python\Python310\python setup.py bdist_wheel
+set PYO3_PYTHON C:\Python\Python311\python.exe
 C:\Python\Python311\python setup.py bdist_wheel
+set PYO3_PYTHON C:\Python\Python312\python.exe
 C:\Python\Python312\python setup.py bdist_wheel
+set PYO3_PYTHON C:\Python\Python313\python.exe
 C:\Python\Python313\python setup.py bdist_wheel
