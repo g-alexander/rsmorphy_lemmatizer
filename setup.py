d752fd3b554021b185dfde97a27674a2776f0d14
g-alexander <g.alexander.box@gmail.com>
2025-08-02 16:41:06 +0300

    Update windows build script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="rsmorphy_lemmatizer",
-    version="0.1.6",
+    version="0.1.7",
     rust_extensions=[RustExtension("rsmorphy_lemmatizer.rsmorphy_lemmatizer", binding=Binding.PyO3)],
     packages=["rsmorphy_lemmatizer"],
     # rust extensions are not zip safe, just like C-extensions.
